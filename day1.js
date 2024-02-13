66c7b7a2d30b2c84e0fd143b0c78cb85df11b901
Raowl2002 <rahultartemh@gmail.com>
2024-02-13 23:44:15 +0530

    fixed the error of error display

--- day1.js
+++ day1.js
@@ -7,18 +7,19 @@ async function getData(cityName){
     try{
         const response = await fetch(apiUrl +`&appid=${apiKey}&q=${cityName}`);
         let data = await response.json();
-        console.log(data);
+        
         document.querySelector(".weather").style.display="block";
+        document.querySelector(".error").style.display ="none";
 
         const weather =data.weather[0].main.toLowerCase();
         document.querySelector(".weather img").setAttribute("src",`images/${weather}.png`);
-        
+
         document.querySelector(".weather h2").textContent=`${cityName[0].toUpperCase()+cityName.slice(1).toLowerCase()} `;
         document.querySelector(".weather .temp").textContent = `${Math.round(data.main.temp)} °C`;
         document.querySelector(".col1 .humidity").textContent =`${data.main.humidity}%`;
         document.querySelector(".col1 .wind").textContent =`${data.wind.speed} km/hr` 
     }catch(error){
-        console.log(`Download error : ${error.message} `);
+
         document.querySelector(".error").style.display ="block";
         document.querySelector(".weather").style.display="none";
     }
